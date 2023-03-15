9e1c204ebba56ee3d603b236a3b1b4b6bab14f52
C'est une pomme <dmgordienko@gmail.com>
2023-03-15 18:05:43 +0400

    Initial Commit

---
+++ iTest/iTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTestApp.swift
+//  iTest
+//
+//  Created by Дмитрий Гордиенко on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
